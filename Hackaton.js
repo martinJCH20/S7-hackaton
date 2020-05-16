ff854d8182020e87a9dea994cb9cd347742e33e2
martinJCH20 <61629740+martinJCH20@users.noreply.github.com>
2020-05-16 09:46:00 -0500

    Add files via upload

--- Hackaton.js
+++ Hackaton.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, FlatList, Dimensions, TouchableOpacity } from 'react-native'
 
-
+let screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
-    container: {
+    containerSlider: {
         flex: 1,
-        backgroundColor: '#ffffff'
+        width: screenWidth,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    menu:{
+        height: '10%',
+        flexDirection: 'row'
     }
 })
+
+const menu = [
+    {id: 1, title: 'Step 1'},
+    {id: 2, title: 'Step 2'},
+    {id: 3, title: 'Step 3'},
+]
+
 export default class Hackaton extends Component {
     constructor(props){
         super(props);
@@ -18,9 +31,30 @@ export default class Hackaton extends Component {
     render(){
         const { postList } = this.state;
         return(
-            <View style={styles.container}>
-               <Text>Hola</Text>
-            </View>
+            <>
+               <ScrollView 
+               horizontal={true}
+               pagingEnabled={true}
+               showsHorizontalScrollIndicator={true}
+               >
+               <FlatList
+                    data={menu}
+                    renderItem={({item}) => (
+                        <>
+                        <Text  style={styles.containerSlider}>{item.title}</Text>
+                         </>
+                    )}
+                    keyExtractor={item => item.id}
+                    numColumns={1}
+                    horizontal={true}
+                />
+                </ScrollView>
+                <View style={styles.menu}>
+                <View  style={{flex: 1, backgroundColor: 'blue'}}><Text>Tips</Text></View >
+                <View  style={{flex: 1, backgroundColor: 'blue'}}><Text>Home</Text></View >
+                <View  style={{flex: 1, backgroundColor: 'blue'}}><Text>Profile</Text></View >
+                </View>
+            </>
         )
     }
 }
